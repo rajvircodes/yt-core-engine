1de5df5ae3458b38211d3552984824512f8ae450
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-02-18 16:06:11 +0530

    some foundation files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import connectDB from "./db/db.js";
+import { app } from './app.js';
+
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000, () => { 
+    console.log(`Server is running at port : ${process.env.PORT}`);
+});
+})
