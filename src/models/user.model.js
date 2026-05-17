e82bf86539e685af82505858ccaff9472e4a8b51
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-05-17 23:58:28 +0530

    password hash

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -45,4 +45,11 @@ const userSchema = new Schema({
     }
 },{timestamps:true})
 
+userSchema.pre('save', async function(next){
+    if(!this.isModified) return next();
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+
 export const User = mongoose.model('User', userSchema)
